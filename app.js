5e73cd0a59bdbeb4b7fe84bce9e4161fb8921a87
feliperosenek <feliperosenek@gmail.com>
2023-04-04 14:53:33 -0300

    deploy

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ async function app() {
             console.log(req.body)
             res.status(200).end()
 
-            if (req.body.login) {
+            if (req.body.login === true) {
                 await page.goto("https://my.soundful.com/")
 
                 await delay(3000)
