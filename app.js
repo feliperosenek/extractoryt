3ac1a757ae11805435f74daba0053e3c5f5cc535
feliperosenek <feliperosenek@gmail.com>
2023-04-05 08:30:52 -0300

    deploy

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ async function app() {
         var options = {
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
             ignoreDefaultArgs: ['--disable-extensions'],
-            headless: false,
+            headless: true,
             userDataDir: './ChromeSession'
           };
 
